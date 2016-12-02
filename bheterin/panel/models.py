50fd8d9efd65e52e5e7e036608ecf3ac586e55c5
ad1cted <ad1cted@hack.cl>
2016-12-02 18:51:45 -0300

    con "modelo" de "mascota" funcional

--- bheterin/panel/models.py
+++ bheterin/panel/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+#modelos
+
+#modelo mascota
+class mascota(models.Model):
+    idmascota= models.AutoField(primary_key=True) # Llave primaria y auto incremental
+    nombre=models.CharField(max_length=50) #nombre de la mascota como maximo 50 caracteres
+    edad=models.IntegerField() # edad de tipo int
+    peso=models.FloatField() #peso flotante
+
+
+    ### FALTA EL DUEÑO AQUI FK!!!!!!!
+
+
