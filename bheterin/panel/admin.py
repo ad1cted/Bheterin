d3b428950b6168748e8648224e88a5f28bb39def
ad1cted <ad1cted@hack.cl>
2016-12-03 23:33:52 -0300

    mas modelos agregados , veterinario y dueño ,faltan relaciones eso si :c

--- bheterin/panel/admin.py
+++ bheterin/panel/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 from .models import mascota
+from .models import veterinario
+from .models import dueño
+
+#acceso a modificar via crud admin
 admin.site.register(mascota)
+admin.site.register(veterinario)
+admin.site.register(dueño)
--- bheterin/panel/models.py
+++ bheterin/panel/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 
+
+"""
+hay que hacer herencia  con dueño y veterinario hacia ->>>>>> persona
+esto funcioanra pero no se ve muy LINDO
+
+"""
+
+
 #modelos
+
+#modelo dueño
+class dueño(models.Model):
+    rutdueño=models.IntegerField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    correo=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=50)
+
 
 #modelo mascota
 class mascota(models.Model):
@@ -8,8 +25,13 @@ class mascota(models.Model):
     nombre=models.CharField(max_length=50) #nombre de la mascota como maximo 50 caracteres
     edad=models.IntegerField() # edad de tipo int
     peso=models.FloatField() #peso flotante
+    # FALTA EL DUEÑO AQUI FK!!!!!!!
 
 
-    ### FALTA EL DUEÑO AQUI FK!!!!!!!
-
-
+#modelo veterinario
+class veterinario(models.Model):
+    rutveterinario=models.IntegerField(primary_key=True)
+    nombre=models.CharField(max_length=50)
+    direccion=models.CharField(max_length=50)
+    correo=models.CharField(max_length=50)
+    telefono=models.CharField(max_length=50)
