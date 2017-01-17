6d2e620ae4151d30d7348028196e377f123dfdde
Netrhil <saragar.jim@gmail.com>
2017-01-17 19:41:13 -0300

    Agregada url de login (comentado por si acaso)

--- bheterin/bheterin/urls.py
+++ bheterin/bheterin/urls.py
@@ -8,17 +8,22 @@ Function views
     2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='
+    home')
 Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+#from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^jet/', include('jet.urls', 'jet')),  # Django JET URLS
     url(r'^jet/dashboard/', include('jet.dashboard.urls', 'jet-dashboard')),
     url(r'^admin/', admin.site.urls),
     url(r'^django-sb-admin/', include('django_sb_admin.urls')),
+    #url(r'^accounts/login/$', auth_views.login,
+    #{'template_name': 'django_sb_admin/examples/login.html'}),
+
 ]
