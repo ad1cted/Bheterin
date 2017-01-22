61d3b7856eafdb57cc5a6ac6d545b68d4e73823a
Netrhil <saragar.jim@gmail.com>
2017-01-22 04:12:55 -0300

    Agregando login

--- bheterin/panel/views.py
+++ bheterin/panel/views.py
@@ -5,5 +5,5 @@ from django.template import loader
 # Create your views here.
 
 def index(request):
-    template = loader.get_template("index.html")
+    template = loader.get_template("login.html")
     return HttpResponse(template.render(request))
