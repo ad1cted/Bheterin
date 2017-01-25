449cf9e4112c1ef0a6e85c53c95913ac7dfbe3df
Netrhil <saragar.jim@gmail.com>
2017-01-25 02:34:35 -0300

    commit c

--- bheterin/panel/urlsVeterinarios.py
+++ bheterin/panel/urlsVeterinarios.py
@@ -3,6 +3,6 @@ from . import views
 #from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-       url(r'^add_veterinario$', views.add_mascota, name="add_veterinario"),
-       url(r'^panel_veterinario$', views.panel_mascota, name="panel_veterinario"),
+       url(r'^add_veterinario$', views.add_veterinario, name="add_veterinario"),
+       url(r'^panel_veterinario$', views.panel_veterinario, name="panel_veterinario"),
       ]
